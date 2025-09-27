91651b96f68a6c4fef50ad464fa733516908cc7a
Amir Hamza Badal <user@Amirs-MacBook-Pro.local>
2025-09-27 21:46:54 +0600

    Initial commit for LcmApi

--- obj/Release/net9.0/LcmApi.AssemblyInfo.cs
+++ obj/Release/net9.0/LcmApi.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("LcmApi")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc31f56bc397b49232e66f260b41ad6a66aa9666")]
 [assembly: System.Reflection.AssemblyProductAttribute("LcmApi")]
